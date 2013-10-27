ffd24d999424a290a151e1f4d86d1f5078a685ab
German Romarion <grromarion@hotmail.com>
2013-10-27 17:42:46 -0300

    analyzer de los chicos llamado processor apenas empezado

---
+++ protos/src/main/java/it/itba/edu/ar/protos/proxy/Processor.java
@@ -0,0 +1,43 @@
+package it.itba.edu.ar.protos.proxy;
+
+import it.itba.edu.ar.protos.model.Data;
+import it.itba.edu.ar.protos.model.HttpPacket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import nl.bitwalker.useragentutils.UserAgent;
+
+public class Processor {
+
+	private Socket socket;
+	private OutputStream clientOS;
+	private InputStream clientIS;
+	private UserAgent ua;
+	private HttpPacket packet;
+
+	public Processor() {
+		packet = new HttpPacket() {
+		};
+	}
+
+	public void process(Data data, Socket socket) {
+		this.socket = socket;
+		boolean hasResponse = processRequest(data);
+
+	}
+
+	public boolean processRequest(Data data) throws IOException {
+		clientOS = socket.getOutputStream();
+		clientIS = socket.getInputStream();
+
+		// Si por alguna razón el parseo de headers fallara, hay que lanzar un
+		// error 501, por ejemplo...
+
+		ua = UserAgent.parseUserAgentString(packet.getHeader("User-Agent"));
+		
+		packet.getHeaders();
+	}
+}
