50247295f8046472c6ed65ee2e3df9ded90dedeb
German Romarion <grromarion@hotmail.com>
2013-11-05 12:40:39 -0300

    actualizado Attachment.java para que funque analogo al del Agulo

--- protos/src/main/java/it/itba/edu/ar/protos/attachment/Attachment.java
+++ protos/src/main/java/it/itba/edu/ar/protos/attachment/Attachment.java
@@ -6,6 +6,7 @@ import it.itba.edu.ar.protos.model.Request;
 import it.itba.edu.ar.protos.model.Response;
 
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 public class Attachment {
 	private State state = State.INIT;
@@ -14,43 +15,74 @@ public class Attachment {
 	private ByteBuffer buffer;
 	private HttpPacket packet;
 	private int packetSize;
-	
+
+	private SocketChannel server;
+	private SocketChannel client;
+
+	private int serverport;
+	private int clientport;
+	private String serveraddr;
+	private String clientaddr;
+
 	private int bufferSize = 1024;
-	
+
 	public Attachment() {
 		lineBuffer = new byte[bufferSize];
 		lineBufferIndex = 0;
 		buffer = ByteBuffer.allocate(bufferSize);
 	}
-	
+
+	public void setServer(SocketChannel server) {
+		this.server = server;
+		serverport = server.socket().getPort();
+		serveraddr = server.socket().getInetAddress().getHostName();
+		buffer = ByteBuffer.allocate(bufferSize);
+	}
+
+	public void setClient(SocketChannel client) {
+		this.client = client;
+		clientport = client.socket().getPort();
+		clientaddr = client.socket().getInetAddress().getHostName();
+		buffer = ByteBuffer.allocate(bufferSize);
+	}
+
+	public SocketChannel getOppositeChannel(SocketChannel channel) {
+		boolean isserver = channel.socket().getPort() == server.socket()
+				.getPort()
+				&& channel.socket().getInetAddress().getHostName()
+						.equals(server.socket().getInetAddress().getHostName());
+		return isserver ? client : server;
+	}
+
 	public void determinePacketType(String op) {
-		if(op.startsWith("GET") || op.startsWith("POST") || op.startsWith("HEAD")) {
+		if (op.startsWith("GET") || op.startsWith("POST")
+				|| op.startsWith("HEAD")) {
 			packet = new Request();
 		} else {
 			packet = new Response();
 		}
 	}
-	
+
 	public State getState() {
 		return state;
 	}
-	
+
 	public void setState(State state) {
 		this.state = state;
 	}
-	
+
 	public ByteBuffer getBuffer() {
 		return buffer;
 	}
-	
+
 	public void setLineBufferIndex(int i) {
 		lineBufferIndex = i;
 	}
-	
+
 	public byte[] getLineBuffer() {
 		return lineBuffer;
 	}
-	
+
 	public int getLineBufferIndex() {
 		return lineBufferIndex;
 	}
@@ -59,12 +91,20 @@ public class Attachment {
 		lineBuffer[i] = c;
 	}
 
+	public SocketChannel getServer() {
+		return server;
+	}
+
+	public SocketChannel getClient() {
+		return client;
+	}
+
 	public void incrementLineBufferIndex(int i) {
 		lineBufferIndex += i;
 		if (lineBufferIndex == this.bufferSize) {
 			this.bufferSize *= 2;
 			byte[] auxBuffer = new byte[this.bufferSize];
-			for (int j = 0 ; i < this.lineBufferIndex ; i++) {
+			for (int j = 0; i < this.lineBufferIndex; i++) {
 				auxBuffer[j] = this.lineBuffer[j];
 			}
 			this.lineBuffer = auxBuffer;
@@ -74,12 +114,28 @@ public class Attachment {
 	public HttpPacket getPacket() {
 		return packet;
 	}
-	
+
 	public int getPacketSize() {
 		return packetSize;
 	}
-	
+
 	public void incrementPacketSize(int amount) {
 		packetSize += amount;
+	}
+
+	public int getServerport() {
+		return serverport;
+	}
+
+	public int getClientport() {
+		return clientport;
+	}
+
+	public String getServeraddr() {
+		return serveraddr;
+	}
+
+	public String getClientaddr() {
+		return clientaddr;
 	}
 }
