6aee9d1a8b2a642a3b100e0d8394d7c6d54ee522
German Romarion <grromarion@hotmail.com>
2013-11-09 22:30:54 -0300

    se agrego javadoc a Response y a Leeter

--- protos/src/main/java/it/itba/edu/ar/protos/model/Leeter.java
+++ protos/src/main/java/it/itba/edu/ar/protos/model/Leeter.java
@@ -1,7 +1,25 @@
 package it.itba.edu.ar.protos.model;
 
+/**
+ * This class is used to transform an input text onto l33t format. It changes
+ * the a's for 4's, the e's for 3's, the i's for 1's, the o's for 0's and the
+ * c's for <'s.
+ *
+ */
 public class Leeter {
 
+/**
+	 * This is the only method offered by the Leeter class. It receives a
+	 * String input and returns a new one changing the a's for 4's, the e's for
+	 * 3's, the i's for 1's, the o's for 0's and the * c's for <'s from the
+	 * original.
+	 * 
+	 * @param text The original text input.
+	 * @return
+	 * 			A new one changing the a's for 4's, the e's for 3's, the i's 
+	 * for 1's, the o's for 0's and the * c's for <'s from the
+	 * original.
+	 */
 	public static String leetify(String text) {
 		text = text.replaceAll("e", "3");
 		text = text.replaceAll("a", "4");
--- protos/src/main/java/it/itba/edu/ar/protos/model/Response.java
+++ protos/src/main/java/it/itba/edu/ar/protos/model/Response.java
@@ -2,18 +2,23 @@ package it.itba.edu.ar.protos.model;
 
 import java.nio.ByteBuffer;
 
+/**
+ * This is a subclass of HttpPacket and it contains all the information related
+ * to an HTTP response, such as the status code and the status message of such
+ * response.
+ */
 public class Response extends HttpPacket {
 	private String status;
 	private String statusMessage;
-	
+
 	@Override
 	public boolean parseFirstLine(String first) {
 		int spaces = 0;
 		int startFrom = 0;
-		
-		for(int i=0 ; i<first.length() ; i++) {
-			if(first.charAt(i) == ' ') {
-				switch(spaces) {
+
+		for (int i = 0; i < first.length(); i++) {
+			if (first.charAt(i) == ' ') {
+				switch (spaces) {
 				case 0:
 					setHttpVersion(first.substring(startFrom, i));
 					break;
@@ -24,26 +29,25 @@ public class Response extends HttpPacket {
 					statusMessage = first.substring(startFrom, i);
 					break;
 				}
-				startFrom = i+1;
+				startFrom = i + 1;
 				spaces++;
 			}
 		}
-		return validateFirstLine();	
+		return validateFirstLine();
 	}
-	
-	
-	private boolean validateFirstLine(){
-		return true;//TODO implement validator
+
+	private boolean validateFirstLine() {
+		return true;// TODO implement validator
 	}
-	
+
 	@Override
 	public boolean validateHeader(String[] header) {
-		return true;//TODO implement validator
+		return true;// TODO implement validator
 	}
 
 	@Override
 	public void generateFirstLine(ByteBuffer packet) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
